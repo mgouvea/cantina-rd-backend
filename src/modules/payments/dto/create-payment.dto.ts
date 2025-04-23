b2e0f72e3902e2eb814a58e6b2a3506f515b4fa9
Mateus Gouvea <cast.mateus.utic@sebrae.com.br>
2025-04-23 15:19:11 -0300

    feat(Payment): Create a new Resource
    - Send the payments to the owner of the group family

--- src/modules/payments/dto/create-payment.dto.ts
+++ src/modules/payments/dto/create-payment.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePaymentDto {}
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreatePaymentDto {
+  invoiceId: string;
+  amountPaid: number;
+  paymentDate: Date;
+  isPartial: boolean;
+  isCredit: boolean;
+  createdAt: Date;
+}
+
+export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {}
--- src/modules/payments/payments.controller.ts
+++ src/modules/payments/payments.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PaymentsService } from './payments.service';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { CreatePaymentDto, UpdatePaymentDto } from './dto/create-payment.dto';
 
 @Controller('payments')
 export class PaymentsController {
@@ -19,16 +26,16 @@ export class PaymentsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.paymentsService.findOne(+id);
+    return this.paymentsService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
-    return this.paymentsService.update(+id, updatePaymentDto);
+    return this.paymentsService.update(id, updatePaymentDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.paymentsService.remove(+id);
+    return this.paymentsService.remove(id);
   }
 }
