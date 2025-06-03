68e8e9650e70b48e4f5f7ca9477f472a21bfa293
Mateus Gouvea <falamateus.gouvea@gmail.com>
2025-06-02 21:46:10 -0300

    feat: implement credit module with MongoDB integration and group family lookup

--- src/modules/credit/credit.module.ts
+++ src/modules/credit/credit.module.ts
@@ -3,10 +3,17 @@ import { CreditService } from './credit.service';
 import { CreditController } from './credit.controller';
 import { Credit, CreditSchema } from './entities/credit.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import {
+  GroupFamily,
+  GroupFamilySchema,
+} from '../group-family/entities/group-family.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Credit.name, schema: CreditSchema }]),
+    MongooseModule.forFeature([
+      { name: Credit.name, schema: CreditSchema },
+      { name: GroupFamily.name, schema: GroupFamilySchema },
+    ]),
   ],
   controllers: [CreditController],
   providers: [CreditService],
--- src/modules/credit/credit.service.ts
+++ src/modules/credit/credit.service.ts
@@ -3,11 +3,17 @@ import { CreateCreditDto, UpdateCreditDto } from './dto/create-credit.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Credit, CreditDocument } from './entities/credit.entity';
+import {
+  GroupFamily,
+  GroupFamilyDocument,
+} from '../group-family/entities/group-family.entity';
 
 @Injectable()
 export class CreditService {
   constructor(
     @InjectModel(Credit.name) private creditModel: Model<CreditDocument>,
+    @InjectModel(GroupFamily.name)
+    private groupFamilyModel: Model<GroupFamilyDocument>,
   ) {}
 
   create(createCreditDto: CreateCreditDto) {
@@ -15,8 +21,26 @@ export class CreditService {
     return credit.save();
   }
 
-  findAll() {
-    return this.creditModel.find().exec();
+  async findAll() {
+    const credits = await this.creditModel.find().lean();
+
+    // Adicionar o nome do grupo familiar para cada crédito
+    const creditsWithGroupName = await Promise.all(
+      credits.map(async (credit) => {
+        const groupFamily = await this.groupFamilyModel
+          .findById(credit.groupFamilyId)
+          .lean();
+
+        return {
+          ...credit,
+          groupFamilyName: groupFamily
+            ? groupFamily.name
+            : 'Grupo não encontrado',
+        };
+      }),
+    );
+
+    return creditsWithGroupName;
   }
 
   findOne(id: string) {
